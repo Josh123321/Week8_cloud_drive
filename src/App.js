326f6f15c9f32e98ff416542d21e112aac1560a4
Josh <gameoveru321@gmailc.om>
2019-09-08 23:50:55 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Logo from './assest/logo.svg';
+import Search from './assest/baseline_search_white_24dp.png';
+import SearchBar from './assest/search bar.svg';
+import UserIcon from './assest/photo_user@2x.png';
+
+const HeadBarWrapper = styled.div`
+  display:flex;
+  align-content:center;
+  justify-content: space-between;
+  margin:30px;
+
+  img {
+    pointer-events:none;
+  }
+  
+  > img:last-child {
+    width:50px;
+    height:50px;
+  }
+`;
+
+const SearchBarWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+`;
+
+const ContentWrapper = styled.div`
+  display:flex;
+  justify-content:space-between;
+  margin:30px;
+
+  > div {
+    border:1px #FFFFFF solid;
+    border-radius:5px;
+    background:#FFFFFF;
+  }
+`;
+
+const TableWrapper = styled.div`
+  height:500px;
+  width:75vw;
+`;
+
+const InfoBlock = styled.div`
+  height:500px;
+  width:15vw;
+  padding-left:30px;
+  display:flex;
+`;
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeadBarWrapper>
+        <img src={Logo} />
+        <SearchBarWrapper>
+          <img src={SearchBar} />
+        </SearchBarWrapper>
+        <img src={UserIcon} />
+      </HeadBarWrapper>
+
+      <ContentWrapper>
+        <TableWrapper>
+
+        </TableWrapper>
+        <InfoBlock>
+          <h3>Hello</h3>
+
+        </InfoBlock>
+      </ContentWrapper>
     </div>
   );
 }
