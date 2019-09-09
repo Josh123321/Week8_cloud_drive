aaca6a3f03844f54e1454df9b52f010350d14a15
Josh Wu <josh.wu@tradevan.com.tw>
2019-09-09 10:21:15 +0800

    Updates

---
+++ precache-manifest.bfa97a8ab516f9b8fef3b5a86939721c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45d6fd7170c94c3eb0756cd07520e59",
+    "url": "/Week8_cloud_drive/index.html"
+  },
+  {
+    "revision": "c01c990881b594c05a92",
+    "url": "/Week8_cloud_drive/static/css/main.6b2da687.chunk.css"
+  },
+  {
+    "revision": "eeb9e33b54505ef34ef2",
+    "url": "/Week8_cloud_drive/static/js/2.8dffb8af.chunk.js"
+  },
+  {
+    "revision": "c01c990881b594c05a92",
+    "url": "/Week8_cloud_drive/static/js/main.66be95cb.chunk.js"
+  },
+  {
+    "revision": "2c06bb0f1dab4ac26ca8",
+    "url": "/Week8_cloud_drive/static/js/runtime~main.00a4e271.js"
+  },
+  {
+    "revision": "d92bf6cbbe310541f8bcbcc9d314da86",
+    "url": "/Week8_cloud_drive/static/media/logo.d92bf6cb.svg"
+  },
+  {
+    "revision": "fdec6789c4de1100890e2cb98e4c2098",
+    "url": "/Week8_cloud_drive/static/media/photo_user@2x.fdec6789.png"
+  },
+  {
+    "revision": "e0e2aea18c17966a780f174481d25180",
+    "url": "/Week8_cloud_drive/static/media/search bar.e0e2aea1.svg"
+  }
+]);
